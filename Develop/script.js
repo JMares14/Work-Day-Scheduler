270bf747c3c2975e559ebf1b1f39940a3310ad76
JM <georgemares14@yahoo.com>
2022-06-21 13:34:46 -0700

    Added comments to understand javascript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,16 @@
+//Got current date for header with moment.js
 var currentDay = moment().format('MMMM Do YYYY');
 document.getElementById("currentDay").innerHTML = currentDay;
 
+//Got current hour for use in loop with moment.js
 var currentHour = moment().hour();
 console.log(currentHour);
 
+// Looped through time-sections to get value of id which equals to timeBlock hour
 $(".time-section").each(function () {
     var timeBlock = parseInt($(this).attr("id"));
 
+   //if else statements to determine the color of timeBlocks depending on the time
     if (timeBlock < currentHour){
         $(this).addClass("past");
         $(this).removeClass("future");
@@ -24,6 +28,7 @@ $(".time-section").each(function () {
     }
 })
 
+//on save-btn click add timeBlock data to localStorage
 $(".save-btn").on("click", function() {
     var time = $(this).parent().attr("id");
     var scheduledText = $(this).siblings(".scheduled").val();
@@ -31,6 +36,7 @@ $(".save-btn").on("click", function() {
     localStorage.setItem(time, scheduledText);
 })
 
+//Pulls saved data from localStorage if available
 $("#9 .scheduled").val(localStorage.getItem("9"));
 $("#10 .scheduled").val(localStorage.getItem("10"));
 $("#11 .scheduled").val(localStorage.getItem("11"));
