6752f74e817da9c233df77146508b019675fa913
JM <georgemares14@yahoo.com>
2022-06-21 12:59:31 -0700

    Made loop with jquery to correctly change color of time blocks depending on the time. Used moment.js to get current hour.

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,25 @@
-let currentDay = moment().format('MMMM Do YYYY');
+var currentDay = moment().format('MMMM Do YYYY');
 document.getElementById("currentDay").innerHTML = currentDay;
+
+var currentHour = moment().hour();
+console.log(currentHour);
+
+$(".time-section").each(function () {
+    var timeBlock = parseInt($(this).attr("id"));
+
+    if (timeBlock < currentHour){
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else if (timeBlock === currentHour) {
+        $(this).addClass ("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+    }
+})
