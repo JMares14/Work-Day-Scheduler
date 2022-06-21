1a2328beb1fa04dd0ba5c9a208ee2e6237a237fd
JM <georgemares14@yahoo.com>
2022-06-21 13:18:01 -0700

    Set time and scheduled text to local storage on button click. Added selectors to pull local storage.

--- Develop/script.js
+++ Develop/script.js
@@ -23,3 +23,21 @@ $(".time-section").each(function () {
         $(this).removeClass("present");
     }
 })
+
+$(".save-btn").on("click", function() {
+    var time = $(this).parent().attr("id");
+    var scheduledText = $(this).siblings(".scheduled").val();
+
+    localStorage.setItem(time, scheduledText);
+})
+
+$("#9 .scheduled").val(localStorage.getItem("9"));
+$("#10 .scheduled").val(localStorage.getItem("10"));
+$("#11 .scheduled").val(localStorage.getItem("11"));
+$("#12 .scheduled").val(localStorage.getItem("12"));
+$("#13 .scheduled").val(localStorage.getItem("13"));
+$("#14 .scheduled").val(localStorage.getItem("14"));
+$("#15 .scheduled").val(localStorage.getItem("15"));
+$("#16 .scheduled").val(localStorage.getItem("16"));
+$("#17 .scheduled").val(localStorage.getItem("17"));
+
